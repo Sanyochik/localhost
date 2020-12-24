e906217953696f9d13586f22eadd72c220b967ef
Sanyochik <sasha_ump@mail.ru>
2020-12-24 15:20:56 +0300

    Initial commit

---
+++ views/site/search.php
@@ -0,0 +1,14 @@
+
+    <div class="container mt-5">
+        <h2 class="text-center">Поиск информации</h2>
+        <div class="row">
+            <div class="col-12 text-center">
+            <input type="text" id="1" class="form-control mt-2" placeholder="Информация 1">
+            <input type="text" id="2" class="form-control mt-2" placeholder="Информация 2">
+            <input type="text" id="3" class="form-control mt-2" placeholder="Информация 3">
+
+                <button type="submit" id="feedbackSubmit" class="btn btn-secondary btn-lg mt-4">Проверить</button>
+
+            </div>
+        </div>
+    </div>
